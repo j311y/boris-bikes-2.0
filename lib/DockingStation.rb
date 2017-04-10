e00989f1075e83b8cc4db0fcc44ac5cad73c5dde
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-10 18:02:05 +0100

    Updated DockingStation so release_bike creates a new instance of Bike. Added relevant Rspec tests.

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,8 +1,9 @@
+require_relative 'Bike'
+
 class DockingStation
 
-
   def release_bike
-
+    Bike.new
   end
 
   def working?
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,3 +4,8 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to :working? }
 end
+
+describe :release_bike do
+  it { expect :release_bike == Bike.new }
+  it { expect Bike.new.respond_to? :working? }
+end
