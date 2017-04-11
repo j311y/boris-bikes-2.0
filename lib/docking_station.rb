e3677c921ce110b366707e241caa77437ef83b32
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-11 17:16:51 +0100

    Added a guard condition that releases bikes only when available, relevant rspec tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,12 @@
-require_relative 'Bike'
+require_relative 'bike'
 
 class DockingStation
 
 attr_reader :bike
 
-
   def release_bike
-    Bike.new
+    fail("No bikes available") unless @bike
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,13 @@
-require "DockingStation"
+require "docking_station"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to :working? }
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to :bike }
+  it "should raise" do
+    expect{:bike.non_existent_message}.to raise_error("No bikes available")
+  end
 end
 
 describe :release_bike do
