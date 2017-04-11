25504054e180f5b00ee4227aeebf660fadb27ab3
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-11 18:10:43 +0100

    added a guard condition to not allow more bikes if dock is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,12 +5,19 @@ describe DockingStation do
   it { is_expected.to respond_to :working? }
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to :bike }
-  it "should raise" do
-    expect{:bike.non_existent_message}.to raise_error("No bikes available")
-  end
-end
 
-describe :release_bike do
-  it { expect :release_bike == Bike.new }
-  it { expect Bike.new.respond_to? :working? }
+  describe ':release_bike' do
+    it { expect :release_bike == Bike.new }
+    it { expect Bike.new.respond_to? :working? }
+    it "raises an error when there are no bikes available" do
+      expect{ subject.release_bike }.to raise_error "No bikes available"
+    end
+  end
+
+  describe ':dock' do
+    it "raise a error when full" do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error "Docking station full"
+    end
+  end
 end
